ce6055fd59681e57ae0679b8205bbf98ad50a17c
Dakota DeGolyer <hq2678@wayne.edu>
2025-09-10 14:29:53 -0400

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,4 +1,6 @@
-// Simple C++ program to display "Hello World" 
+// Simple C++ program to display "Hello World"
+// Author: Dakota DeGolyer <hq2678@wayne.edu>
+
 // Header file for input output functions 
 #include<iostream>  
   
